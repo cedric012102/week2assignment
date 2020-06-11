cfce1b790cdb309843af8b3546b6ad822d689b1f
cedric smith <cedric012102@gmail.com>
2020-06-11 11:23:08 -0700

    what I created

---
+++ index.js
@@ -0,0 +1,30 @@
+let isHotOutside = true;
+let isWeekday = false;
+let hasMoneyInPocket = true;
+
+let costOfMilk = 2;
+let moneyInWallet = 3;
+let thirstyLevel = 5;
+
+let shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
+let willGoSwimming = isHotOutside && isWeekday;
+var isAGoodDay = hasMoneyInPocket && !isWeekday;
+var willBuyMilk = isHotOutside && thirstyLevel >= 2;
+
+if (isHotOutside && hasMoneyInPocket == true) {
+    console.log(shouldBuyIceCream);
+} else {
+    prompt("You do not have enough money!")
+};
+
+if (isHotOutside && isWeekday == false); {
+    console.log("willGoSwimming.");
+}
+
+if (hasMoneyInPocket && !isWeekday); {
+    console.log('isAGoodDay.');
+}
+
+if (isHotOutside && thirstyLevel >= 2) {
+    console.log('willBuyMilk.');
+} 
---
+++ loops.js
@@ -0,0 +1,33 @@
+let i = 0
+
+while(i <= 100) {
+    i += 2;
+    console.log(i);
+   }
+
+   let a = 100
+
+   while(a > 0) {
+      a -= 3;
+      console.log(a)
+   }
+
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i % 3 == 0); {
+        console.log("Hello") 
+    } 
+    if (i % 5 == 0); {
+      console.log("World")  
+    }
+    if (i % 3 == 0 && i % 5 == 0); {
+        console.log("Hello World")
+    }
+}
